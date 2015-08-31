eb7e74664aba0dfb392dfd56acca4cde67a45d28
Michael Drogalis <mjd3089@rit.edu>
2015-08-31 14:19:06 +0000

    Bump.

--- project-a/project.clj
+++ project-a/project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :plugins [[lonocloud/lein-unison "0.1.5"]]
   :unison
-  {:repos [{:git "git@github.com:MichaelDrogalis/test-unison.git"
+  {:repos [{:gitx "git@github.com:MichaelDrogalis/test-unison.git"
             :branch "master"
             :project-file "project-b/project.clj"}]})
