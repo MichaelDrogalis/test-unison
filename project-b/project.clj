db8c9f50819447da04f03c8b3e4cf8c034de1123
Michael Drogalis <mjd3089@rit.edu>
2015-08-31 13:07:04 +0000

    Bump.

--- project-b/project.clj
+++ project-b/project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 ^{:voom {:repo "git@github.com:MichaelDrogalis/test-unison.git" :branch "master"}}
+                 [project-a "0.1.0-20150831_125708-ga8878d6"]])
