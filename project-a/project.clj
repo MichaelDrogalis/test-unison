9d335eb2994af96c842a4013ab7fe9dbd231d8b1
Michael Drogalis <mjd3089@rit.edu>
2015-09-01 19:31:07 +0000

    Bump.

--- project-a/project.clj
+++ project-a/project.clj
@@ -1,5 +1,5 @@
 (defproject project-a "0.1.0-SNAPSHOT"
-  :description "FIXME: write descriptionxx"
+  :description "FIXME: write descriptionxxy"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
