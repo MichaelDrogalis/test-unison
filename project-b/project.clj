71154db40590eef789c5da790fcf496c4c447845
Michael Drogalis <mjd3089@rit.edu>
2015-09-01 20:05:15 +0000

    Update dependency project-a to version 0.1.0-20150901_200454-g4f4d6df.
    Automatic commit by lein-unison.

--- project-b/project.clj
+++ project-b/project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:MichaelDrogalis/test-unison.git" :branch "master"}}
-                 [project-a "0.1.0-20150901_193150-g9d335eb"]])
+                 [project-a "0.1.0-20150901_200454-g4f4d6df"]])
