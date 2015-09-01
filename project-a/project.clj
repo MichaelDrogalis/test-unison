a64042aae90020d30148bf0d0b9269d87d2ae6fb
Michael Drogalis <mjd3089@rit.edu>
2015-09-01 20:09:10 +0000

    Bump.

--- project-a/project.clj
+++ project-a/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :plugins [[lonocloud/lein-unison "0.1.7"]]
+  :plugins [[lonocloud/lein-unison "0.1.8"]]
   :unison
   {:repos [{:git "git@github.com:MichaelDrogalis/test-unison.git"
             :branch "master"
